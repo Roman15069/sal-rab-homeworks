5d58b6a562b2fa5f5466b28bdad57f47b0b64962
Roman <prohorov150690@gmail.com>
2022-12-19 21:50:51 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,19 +4,19 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
-    // какой-нибудь коментарий
-    // Конец решения задания №1.1.
-
-    return message;
+     let message;
+     message = productName;
+     message = message + ' ';
+     message = message + 'за';
+     message = message + ' ';
+     message = message + productPrice;
+     message = message + ' ';
+     message = message 'теперь в корзине!';
+     console.log(message);
+     
+      
+     
+     return message;
 }
 
 function addInCartCountChange(value) {
@@ -24,9 +24,10 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+     let newValue; 
+     newValue = oldValue;
+     newValue = 1;
+     
 
     // Конец решения задания №1.2.
 
